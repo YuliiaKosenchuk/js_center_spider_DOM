b3d420d0736f6f17b1042aba9865277eebef315c
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-08-04 20:34:55 +0200

    make build

---
+++ index.ccfd1938.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),c=e.getBoundingClientRect().height/2,n=Math.round(t.clientHeight/2-c),l=Math.round(t.clientWidth/2-c);e.style.top="".concat(n,"px"),e.style.left="".concat(l,"px");
+//# sourceMappingURL=index.ccfd1938.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const halfSpiderSize = spider.getBoundingClientRect().height / 2;
+const topSpider = Math.round(wall.clientHeight / 2 - halfSpiderSize);
+const leftSpider = Math.round(wall.clientWidth / 2 - halfSpiderSize);
+spider.style.top = `${topSpider}px`;
+spider.style.left = `${leftSpider}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
